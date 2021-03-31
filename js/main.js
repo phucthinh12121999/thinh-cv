74254b1b7ffee0bdd41fe0a9a693c6bce1aafc52
phucthinh12121999 <phucthinh12121999@gmail.com>
2021-03-31 11:30:43 +0700

    change animation code

--- js/main.js
+++ js/main.js
@@ -1,108 +1,71 @@
-
-    // AOS
-    AOS.init({
-      duration: 1200,
-      once: true,
-      disable: 'mobile'
-  });
-jQuery(document).ready(function( $ ) {
+// AOS
+AOS.init({
+  duration: 1200,
+  once: true,
+  disable: "mobile",
+});
+jQuery(document).ready(function ($) {
   $(window).load(function () {
     "use strict";
     // makes sure the whole site is loaded
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(350).css({
-        'overflow': 'visible'
+    $("#status").fadeOut(); // will first fade out the loading animation
+    $("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+    $("body").delay(350).css({
+      overflow: "visible",
     });
-})
-    // Smooth scroll for the menu and links with .scrollto classes
-    $('.smothscroll').on('click', function(e) {
-      e.preventDefault();
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        if (target.length) {
-  
-          $('html, body').animate({
-            scrollTop: target.offset().top - 62
-          }, 1500, 'easeInOutExpo');
-        }
-      }
-    });
-  
-    $('.carousel').carousel({
-      interval: 3500
-    });
-  
-    // JavaScript Chart
-    var doughnutData = [{
-        value: 70,
-        color: "#1abc9c"
-      },
-      {
-        value: 30,
-        color: "#ecf0f1"
-      }
-    ];
-    var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
-  
-    // Bootstrap Chart
-    var doughnutData = [{
-      value: 90,
-      color: "#1abc9c"
-    },
-    {
-      value: 10,
-      color: "#ecf0f1"
-    }
-    ];
-    var myDoughnut = new Chart(document.getElementById("bootstrap").getContext("2d")).Doughnut(doughnutData);
-  
-    // WordPress Chart
-    var doughnutData = [{
-      value: 65,
-      color: "#1abc9c"
-    },
-    {
-      value: 35,
-      color: "#ecf0f1"
-    }
-    ];
-    var myDoughnut = new Chart(document.getElementById("wordpress").getContext("2d")).Doughnut(doughnutData);
-  
-    // HTML Chart
-    var doughnutData = [{
-      value: 80,
-      color: "#1abc9c"
-    },
-    {
-      value: 20,
-      color: "#ecf0f1"
-    }
-    ];
-    var myDoughnut = new Chart(document.getElementById("html").getContext("2d")).Doughnut(doughnutData);
-  
-    // Photoshop Chart
-    var doughnutData = [{
-      value: 70,
-      color: "#1abc9c"
-    },
-    {
-      value: 30,
-      color: "#ecf0f1"
-    }
-    ];
-    var myDoughnut = new Chart(document.getElementById("photoshop").getContext("2d")).Doughnut(doughnutData);
-  
-    // Illustrator Chart
-    var doughnutData = [{
-      value: 50,
-      color: "#1abc9c"
-    },
-    {
-      value: 50,
-      color: "#ecf0f1"
-    }
-    ];
-    var myDoughnut = new Chart(document.getElementById("illustrator").getContext("2d")).Doughnut(doughnutData);
   });
-  
+  // Smooth scroll for the menu and links with .scrollto classes
+  $(".smothscroll").on("click", function (e) {
+    e.preventDefault();
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 62,
+          },
+          1500,
+          "easeInOutExpo"
+        );
+      }
+    }
+  });
+
+  $(".carousel").carousel({
+    interval: 3500,
+  });
+});
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
